d23673cbc85ba03172c5fd942a7d2397179712e8
restjohn <restjohn@users.noreply.github.com>
2019-01-02 15:29:24 -0700

    clean up the original GoogleProjection logic with better names

--- spherical_mercator.py
+++ spherical_mercator.py
@@ -1,38 +1,39 @@
-from math import pi, sin, log, exp, atan
+from math import pi, sin, log, exp, atan, radians, degrees
 
-DEG_TO_RAD = pi/180
-RAD_TO_DEG = 180/pi
+def _constrained_sine(rad):
+    sine_rad = sin(rad)
+    return min(max(sine_rad, -0.9999), 0.9999)
 
-def minmax (a,b,c):
-    a = max(a,b)
-    a = min(a,c)
-    return a
 
-class GoogleProjection:
-    def __init__(self,levels=18):
-        self.Bc = []
-        self.Cc = []
-        self.zc = []
-        self.Ac = []
-        c = 256
-        for d in range(0,levels):
-            e = c/2;
-            self.Bc.append(c/360.0)
-            self.Cc.append(c/(2 * pi))
-            self.zc.append((e,e))
-            self.Ac.append(c)
-            c *= 2
+class ZoomLevelSpec:
 
-    def fromLLtoPixel(self,ll,zoom):
-         d = self.zc[zoom]
-         e = round(d[0] + ll[0] * self.Bc[zoom])
-         f = minmax(sin(DEG_TO_RAD * ll[1]),-0.9999,0.9999)
-         g = round(d[1] + 0.5*log((1+f)/(1-f))*-self.Cc[zoom])
-         return (e,g)
+    def __init__(self, zoom_level, globe_pixels):
+        self.zoom_level = zoom_level
+        self.globe_pixels = globe_pixels
+        self.hemisphere_pixels = globe_pixels / 2
+        self.pixels_per_lon_degree = globe_pixels / 360.0
+        self.pixels_per_lon_radian = globe_pixels / (2.0 * pi)
 
-    def fromPixelToLL(self,px,zoom):
-         e = self.zc[zoom]
-         f = (px[0] - e[0])/self.Bc[zoom]
-         g = (px[1] - e[1])/-self.Cc[zoom]
-         h = RAD_TO_DEG * ( 2 * atan(exp(g)) - 0.5 * pi)
-         return (f,h)
+
+class SphericalMercator:
+
+    def __init__(self, levels=18, globe_pixels_zoom_0=256):
+        self.zoom_level_specs = []
+        globe_pixels = globe_pixels_zoom_0
+        for zoom_level in range(0, levels):
+            self.zoom_level_specs.append(ZoomLevelSpec(zoom_level, globe_pixels))
+            globe_pixels *= 2
+
+    def pixel_for_lonlat(self, ll, zoom):
+        zoom_spec = self.zoom_level_specs[zoom]
+        px = round(zoom_spec.hemisphere_pixels + ll[0] * zoom_spec.pixels_per_lon_degree)
+        sine_lat = _constrained_sine(radians(ll[1]))
+        py = round(zoom_spec.hemisphere_pixels + 0.5 * log((1 + sine_lat) / (1 - sine_lat)) * -zoom_spec.pixels_per_lon_radian)
+        return (px, py)
+
+    def lonlat_for_pixel(self, pixel, zoom):
+        zoom_spec = self.zoom_level_specs[zoom]
+        lon = (pixel[0] - zoom_spec.hemisphere_pixels) / zoom_spec.pixels_per_lon_degree
+        g = (pixel[1] - zoom_spec.hemisphere_pixels) / -zoom_spec.pixels_per_lon_radian
+        lat = degrees(2 * atan(exp(g)) - 0.5 * pi)
+        return (lon, lat)
