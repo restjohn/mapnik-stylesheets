79df2be7147b756ca33f9cab7fb485f7cc4cb940
restjohn <restjohn@users.noreply.github.com>
2019-01-04 15:29:42 -0700

    get script args from environment and download mapnik style from url

--- generate_tiles_multiprocess.py
+++ generate_tiles_multiprocess.py
@@ -3,6 +3,11 @@ from math import pi, cos, sin, log, exp, atan
 from subprocess import call
 import sys
 import os
+import re
+import shutil
+import tempfile
+import urllib.request
+import uuid
 import multiprocessing
 import spherical_mercator
 
@@ -17,6 +22,7 @@ NUM_THREADS = 4
 
 
 class RenderThread:
+
     def __init__(self, tile_dir, mapfile, q, printLock, maxZoom):
         self.tile_dir = tile_dir
         self.q = q
@@ -88,6 +94,7 @@ class RenderThread:
 
 
 def render_tiles(bbox, mapfile, tile_dir, minZoom=1, maxZoom=18, name="unknown", num_threads=NUM_THREADS):
+
     print("render_tiles(", bbox, mapfile, tile_dir, minZoom, maxZoom, name, ")")
 
     # Launch rendering threads
@@ -98,7 +105,7 @@ def render_tiles(bbox, mapfile, tile_dir, minZoom=1, maxZoom=18, name="unknown",
         renderer = RenderThread(tile_dir, mapfile, queue, printLock, maxZoom)
         render_thread = multiprocessing.Process(target=renderer.loop)
         render_thread.start()
-        # print "Started render thread %s" % render_thread.getName()
+        # print("Started render thread %s" % render_thread.getName())
         renderers[i] = render_thread
 
     if not os.path.isdir(tile_dir):
@@ -146,66 +153,21 @@ def render_tiles(bbox, mapfile, tile_dir, minZoom=1, maxZoom=18, name="unknown",
 
 if __name__ == "__main__":
 
-    home = os.environ['HOME']
-    try:
-        mapfile = os.environ['MAPNIK_MAP_FILE']
-    except KeyError:
-        mapfile = home + "/svn.openstreetmap.org/applications/rendering/mapnik/osm-local.xml"
-    try:
-        tile_dir = os.environ['MAPNIK_TILE_DIR']
-    except KeyError:
-        tile_dir = home + "/osm/tiles/"
+    tiles_name = os.environ["TILES_NAME"]
+    tiles_bbox = os.environ["TILES_BBOX"]
+    tiles_style_url = os.environ["TILES_MAPNIK_STYLE"]
+    tiles_min_zoom = int(os.environ.get("TILES_MIN_ZOOM", "0"))
+    tiles_max_zoom = int(os.environ.get("TILES_MAX_ZOOM", "18"))
+    tiles_dir = "/tiles/%(tiles_name)s" % locals()
 
-    if not tile_dir.endswith('/'):
-        tile_dir = tile_dir + '/'
+    bbox_parts = re.findall(r"-?\d+(\.\d+", tiles_bbox)
+    if bbox_parts.count != 4:
+        raise Exception("invalid bbox: %s(bbox)" % tiles_bbox)
+    bbox = [(float(bbox_parts[0]), float(bbox_parts[1])), (float(bbox_parts[2]), float(bbox_parts[3]))]
+    style_path = "/tmp/tiles-%(name)s-%(suffix)s" % {"name": tiles_name, "suffix": uuid.uuid4()}
+    style_file = open(style_path, "w", encoding="utf-8")
+    with urllib.request.urlopen(tiles_style_url) as response:
+        shutil.copyfileobj(response, style_file)
+        style_file.close()
 
-    # -------------------------------------------------------------------------
-    #
-    # Change the following for different bounding boxes and zoom levels
-    #
-    # Start with an overview
-    # World
-    bbox = (-180.0, -90.0, 180.0, 90.0)
-
-    render_tiles(bbox, mapfile, tile_dir, 0, 5, "World")
-
-    minZoom = 10
-    maxZoom = 16
-    bbox = (-2, 50.0, 1.0, 52.0)
-    render_tiles(bbox, mapfile, tile_dir, minZoom, maxZoom)
-
-    # Muenchen
-    bbox = (11.4, 48.07, 11.7, 48.22)
-    render_tiles(bbox, mapfile, tile_dir, 1, 12, "Muenchen")
-
-    # Muenchen+
-    bbox = (11.3, 48.01, 12.15, 48.44)
-    render_tiles(bbox, mapfile, tile_dir, 7, 12, "Muenchen+")
-
-    # Muenchen++
-    bbox = (10.92, 47.7, 12.24, 48.61)
-    render_tiles(bbox, mapfile, tile_dir, 7, 12, "Muenchen++")
-
-    # Nuernberg
-    bbox = (10.903198, 49.560441, 49.633534, 11.038085)
-    render_tiles(bbox, mapfile, tile_dir, 10, 16, "Nuernberg")
-
-    # Karlsruhe
-    bbox = (8.179113, 48.933617, 8.489252, 49.081707)
-    render_tiles(bbox, mapfile, tile_dir, 10, 16, "Karlsruhe")
-
-    # Karlsruhe+
-    bbox = (8.3, 48.95, 8.5, 49.05)
-    render_tiles(bbox, mapfile, tile_dir, 1, 16, "Karlsruhe+")
-
-    # Augsburg
-    bbox = (8.3, 48.95, 8.5, 49.05)
-    render_tiles(bbox, mapfile, tile_dir, 1, 16, "Augsburg")
-
-    # Augsburg+
-    bbox = (10.773251, 48.369594, 10.883834, 48.438577)
-    render_tiles(bbox, mapfile, tile_dir, 10, 14, "Augsburg+")
-
-    # Europe+
-    bbox = (1.0, 10.0, 20.6, 50.0)
-    render_tiles(bbox, mapfile, tile_dir, 1, 11, "Europe+")
+    render_tiles(bbox, style_path, tiles_dir, tiles_min_zoom, tiles_max_zoom)
